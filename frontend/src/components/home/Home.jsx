0a950fa708828ef34b6b8ce37c3ebc7362e1263c
ravichoudhary18 <ravi18choudhary18@gmail.com>
2024-01-20 13:50:22 +0530

    Initial commit

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import { Button } from '@mui/material';
+import React from 'react';
+import useAxiosPrivate from '../../hooks/useAxiosPrivate'
+
+const Home = () => {
+
+    return (
+        <div>
+            <p>Home</p>
+        </div>
+    );
+}
+
+export default Home;
